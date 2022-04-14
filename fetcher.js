c9ccd68be3960d83fa494faf944a65b126c0dd51
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-14 16:42:42 +0000

    page fetcher

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+const args = process.argv.slice(2)
+const fs = require('fs')
+const request = require("request");
+const site = args[0]
+const url = args[1]
+const content = request(site, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    fs.writeFile(url, body, err => {
+        if (err) {
+            console.error(err)
+            return
+            //file written successfully
+        }
+    })  
+});
+
+
+
+//console.log(content);
