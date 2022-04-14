5928eb6e057708da5d88ab423ef4de880e6ea193
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-14 16:47:52 +0000

    fetcher

--- fetcher.js
+++ fetcher.js
@@ -4,8 +4,9 @@ const request = require("request");
 const site = args[0]
 const url = args[1]
 const content = request(site, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log(`'Downloaded and saved ${body.length} bytes to ./index.html'`)
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //console.log('body:', body); // Print the HTML for the Google homepage.
     fs.writeFile(url, body, err => {
         if (err) {
